bbdd9e772d25552a2e94ff189bb69b34b4c6540d
이주현 <ljhp1004@gmail.com>
2024-04-25 21:35:53 +0900

    [SERVER] feat : handle network request

---
+++ server/realtime/src/main/java/com/explorer/realtime/serverManaging/RequestHandler.java
@@ -0,0 +1,27 @@
+package com.explorer.realtime.serverManaging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+import reactor.netty.NettyInbound;
+import reactor.netty.NettyOutbound;
+
+public class RequestHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
+    public Mono<Void> handleRequest(NettyInbound inbound, NettyOutbound outbound) {
+
+        return inbound
+                .receive()                                      // 1) receive data
+                .asString()                                     // 2) convert data : byte -> string
+                .flatMap(msg -> {                               // 3) process data
+
+                    log.info("Received Message: {}", msg);      // 3-1) logging
+                    return outbound.sendString(Mono.just(msg)); // 3-2) echoing
+
+                })
+                .then();                                        // 4) complete reactive sequence
+
+    }
+}
