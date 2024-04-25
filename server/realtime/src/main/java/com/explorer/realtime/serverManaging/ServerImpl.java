a1b40a5ece2ab0c804db2cea8a84b138007075ff
이주현 <ljhp1004@gmail.com>
2024-04-25 21:43:48 +0900

    [SERVER] style : remove unused import statement

--- server/realtime/src/main/java/com/explorer/realtime/serverManaging/ServerImpl.java
+++ server/realtime/src/main/java/com/explorer/realtime/serverManaging/ServerImpl.java
@@ -1,6 +1,5 @@
 package com.explorer.realtime.serverManaging;
 
-import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
