ab16f5317cd21916fd38a17f9773fc18b92455f1
서지원 <sjs3326@naver.com>
2024-04-25 09:52:20 +0900

    [SERVER] feat: access token blacklist 확인 기능 구현

--- server/api-gateway/src/main/java/com/explorer/apigateway/global/jwt/JwtUtils.java
+++ server/api-gateway/src/main/java/com/explorer/apigateway/global/jwt/JwtUtils.java
@@ -1,16 +1,22 @@
 package com.explorer.apigateway.global.jwt;
 
+import com.explorer.apigateway.global.common.dto.TokenInfo;
+import com.explorer.apigateway.global.jwt.repository.BlackListRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
 @RequiredArgsConstructor
 public class JwtUtils {
 
-    private final JwtPros props;
+    private final JwtProps props;
+    private final BlackListRepository blackListRepository;
+
     private static final String CLAIM_NICKNAME = "nickname";
     private static final String CLAIM_AVATAR = "avatar";
 
@@ -29,9 +35,17 @@ public class JwtUtils {
 
     private Claims getPayload(String token) {
         return Jwts.parser()
-                .verifyWith(Keys.hmacShaKeyFor(props.getAccessKey().getBytes()))
+                .verifyWith(Keys.hmacShaKeyFor(props.accessKey().getBytes()))
                 .build()
                 .parseSignedClaims(token).getPayload();
+    }
+
+    public Date getExpirationDate(String token) {
+        return getPayload(token).getExpiration();
+    }
+
+    public boolean isBlackListed(String token) {
+        return blackListRepository.exist(token);
     }
 
 }
