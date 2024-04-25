07e37be992aa3b98932a0b2e800877e5c4c804ea
이주현 <ljhp1004@gmail.com>
2024-04-25 21:18:12 +0900

    [SERVER] feat : start realtime server

---
+++ server/realtime/src/main/java/com/explorer/realtime/serverManaging/ServerImpl.java
@@ -0,0 +1,19 @@
+package com.explorer.realtime.serverManaging;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServerImpl implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(ServerImpl.class);
+    @Override
+    public void run(String... args) throws Exception {
+
+        log.info("Starting Server...");
+
+    }
+}
