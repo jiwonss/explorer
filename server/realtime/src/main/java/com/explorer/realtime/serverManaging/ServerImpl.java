99d4788fb67dfd63c135f0b204e9f5a78a62e2ae
이주현 <ljhp1004@gmail.com>
2024-04-25 21:28:57 +0900

    [SERVER] feat : subscribe TcpServer instance

--- server/realtime/src/main/java/com/explorer/realtime/serverManaging/ServerImpl.java
+++ server/realtime/src/main/java/com/explorer/realtime/serverManaging/ServerImpl.java
@@ -9,11 +9,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class ServerImpl implements CommandLineRunner {
 
+    private final ServerInitializer serverInitializer;
+
+    public ServerImpl(ServerInitializer serverInitializer) {
+        this.serverInitializer = serverInitializer;
+    }
+
     private static final Logger log = LoggerFactory.getLogger(ServerImpl.class);
     @Override
     public void run(String... args) throws Exception {
 
         log.info("Starting Server...");
+
+        serverInitializer.initializeServer().subscribe(
+                disposableServer -> {
+                    log.info("Server started on port: {}", disposableServer.port());
+                },
+                error -> {
+                    log.error("Failed to start Server: {}", error.getMessage());
+                }
+        );
 
     }
 }
