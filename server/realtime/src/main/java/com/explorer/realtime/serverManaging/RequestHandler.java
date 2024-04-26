78ba466f84413981310c9c7be6932a2c7e5edbb9
전재완 <wodhksdl7893@naver.com>
2024-04-26 12:10:42 +0900

    [SERVER] feat: requestHandler 타입 분기

--- server/realtime/src/main/java/com/explorer/realtime/serverManaging/RequestHandler.java
+++ server/realtime/src/main/java/com/explorer/realtime/serverManaging/RequestHandler.java
@@ -1,6 +1,7 @@
 package com.explorer.realtime.serverManaging;
 
 import com.explorer.realtime.global.teamCode.TeamCodeGenerator;
+import com.explorer.realtime.sessionhandling.ingame.IngameSessionHandler;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -16,9 +17,11 @@ public class RequestHandler {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private final TeamCodeGenerator teamCodeGenerator;
+    private final IngameSessionHandler ingameSessionHandler;
 
-    public RequestHandler(TeamCodeGenerator teamCodeGenerator) {
+    public RequestHandler(TeamCodeGenerator teamCodeGenerator, IngameSessionHandler ingameSessionHandler) {
         this.teamCodeGenerator = teamCodeGenerator;
+        this.ingameSessionHandler = ingameSessionHandler;
     }
 
     public Mono<Void> handleRequest(NettyInbound inbound, NettyOutbound outbound) {
@@ -32,37 +35,17 @@ public class RequestHandler {
                         JSONObject json = new JSONObject(msg);      // parse string to json
                         log.info("Received Json Data: {}", json);
 
-                        String event = json.getString("event");
+                        String type = json.getString("type");
 
-                        switch(event) {
-                            case "createWaitingRoom" :
-                                log.info("create waiting room");
-                                teamCodeGenerator.getCode().subscribe(  // get teamCode : just for test!!
-                                        teamCode -> {
-                                            log.info("create waiting room with code {}", teamCode);
-                                        },
-                                        error -> log.error("Error fetching team code", error)
-                                );
-                                break;
+                        switch(type) {
+                                case "waitingRoomSession" :
+                                    log.info("waiting room");
+                                    break;
 
-                            case "joinWaitingRoom":
-                                log.info("join waiting room");
-                                break;
 
-                            case "leaveWaitingRoom":
-                                log.info("leave waiting room");
-                                break;
-
-                            case "startGame" :
+                            case "ingameSession" :
                                 log.info("start game");
-                                break;
-
-                            case "restartGame":
-                                log.info("restart game");
-                                break;
-
-                            case "endGame":
-                                log.info("endGame");
+                                ingameSessionHandler.ingameHandler(json);
                                 break;
                         }
 
