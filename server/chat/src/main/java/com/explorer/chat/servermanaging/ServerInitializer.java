a38eb9e6affb9972cdf2910f7d29558695773d87
이주현 <ljhp1004@gmail.com>
2024-04-30 02:23:02 +0900

    [SERVER] feat: integrate RequestHandler into ServerInitializer for handling TCP requests

--- server/chat/src/main/java/com/explorer/chat/servermanaging/ServerInitializer.java
+++ server/chat/src/main/java/com/explorer/chat/servermanaging/ServerInitializer.java
@@ -1,5 +1,6 @@
 package com.explorer.chat.servermanaging;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
@@ -7,16 +8,20 @@ import reactor.netty.DisposableServer;
 import reactor.netty.tcp.TcpServer;
 
 @Component
+@RequiredArgsConstructor
 public class ServerInitializer {
 
     @Value("${chat.server.port}")
     private int PORT;
+
+    private final RequestHandler requestHandler;
 
     public Mono<? extends DisposableServer> initializeServer() {
 
         return TcpServer
                 .create()
                 .port(PORT)
+                .handle(requestHandler::handleRequest)
                 .bind();
     }
 }
