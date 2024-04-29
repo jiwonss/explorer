f445a9d9048fa2ada83eb1cc97784600a2803298
이주현 <ljhp1004@gmail.com>
2024-04-30 01:44:52 +0900

    [SERVER] feat : integrate ServerInitialize

--- server/chat/src/main/java/com/explorer/chat/servermanaging/ServerImpl.java
+++ server/chat/src/main/java/com/explorer/chat/servermanaging/ServerImpl.java
@@ -10,10 +10,21 @@ public class ServerImpl implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(ServerImpl.class);
 
+    private final ServerInitializer serverInitializer;
+
+    public ServerImpl(ServerInitializer serverInitializer) {
+        this.serverInitializer = serverInitializer;
+    }
+
     @Override
     public void run(String... args) throws Exception {
 
         log.info("Starting Chat Server...");
-        
+
+        serverInitializer.initializeServer().subscribe(
+                disposableServer -> log.info("Chat Server started on port: {}", disposableServer.port()),
+                error -> log.error("Failed to start Chat Server: {}", error.getMessage())
+        );
+
     }
 }
