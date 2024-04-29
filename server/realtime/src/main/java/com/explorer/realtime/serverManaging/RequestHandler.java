46f68e819adb9b4f3a73c9dc3068f7869de1f825
이주현 <ljhp1004@gmail.com>
2024-04-29 16:13:54 +0900

    [SERVER] fix: Modification to merge into the session handling code of the develop branch

--- server/realtime/src/main/java/com/explorer/realtime/serverManaging/RequestHandler.java
+++ server/realtime/src/main/java/com/explorer/realtime/serverManaging/RequestHandler.java
@@ -1,11 +1,7 @@
 package com.explorer.realtime.serverManaging;
 
-import com.explorer.realtime.global.broadcasting.Broadcasting;
-import com.explorer.realtime.global.broadcasting.Multicasting;
-import com.explorer.realtime.global.broadcasting.Unicasting;
-import com.explorer.realtime.global.redis.RedisService;
-import com.explorer.realtime.global.session.SessionManager;
 import com.explorer.realtime.global.teamCode.TeamCodeGenerator;
+import com.explorer.realtime.sessionhandling.ingame.IngameSessionHandler;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -21,19 +17,11 @@ public class RequestHandler {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private final TeamCodeGenerator teamCodeGenerator;
-    private final SessionManager sessionManager;
-    private final RedisService redisService;
-    private final Broadcasting broadcasting;
-    private final Multicasting multicasting;
-    private final Unicasting unicasting;
+    private final IngameSessionHandler ingameSessionHandler;
 
-    public RequestHandler(TeamCodeGenerator teamCodeGenerator, SessionManager sessionManager, RedisService redisService, Broadcasting broadcasting, Multicasting multicasting, Unicasting unicasting) {
+    public RequestHandler(TeamCodeGenerator teamCodeGenerator, IngameSessionHandler ingameSessionHandler) {
         this.teamCodeGenerator = teamCodeGenerator;
-        this.sessionManager = sessionManager;
-        this.redisService = redisService;
-        this.broadcasting = broadcasting;
-        this.multicasting = multicasting;
-        this.unicasting = unicasting;
+        this.ingameSessionHandler = ingameSessionHandler;
     }
 
     public Mono<Void> handleRequest(NettyInbound inbound, NettyOutbound outbound) {
@@ -48,62 +36,20 @@ public class RequestHandler {
                         log.info("Received Json Data: {}", json);
 
                         String type = json.getString("type");
-                        String event = json.getString("event");
 
                         switch(type) {
                                 case "waitingRoomSession" :
-
                                     log.info("waiting room");
-                                    String uid = json.getString("uid");
-
-                                    switch(event) {
-                                        case "createWaitingRoom":
-                                            log.info("create Waiting Room");
-
-                                            return teamCodeGenerator.getCode()
-                                                    .flatMap(teamCode -> {
-                                                        inbound.withConnection(connection -> {
-                                                            sessionManager.setConnection(uid, connection);
-                                                            redisService.saveUidToTeamCode(teamCode, uid, "waitingRoom").subscribe();
-                                                            log.info("uid: {}, teamCode: {}, connection: {}", sessionManager.getUid(connection), teamCode, sessionManager.getConnection(uid));
-                                                        });
-                                                        return Mono.empty();
-                                                    });
-
-                                        case "joinWaitingRoom":
-                                            log.info("join Waiting Room");
-                                            String teamCode_ = json.getString("teamCode");
-                                            inbound.withConnection(connection -> {
-                                                sessionManager.setConnection(uid, connection);
-                                                redisService.saveUidToTeamCode(teamCode_,uid,"waitingRoom").subscribe();
-                                                log.info("uid: {}, teamCode: {}, connection: {}", sessionManager.getUid(connection), sessionManager.getConnection(uid), teamCode_);
-                                                unicasting.unicasting(teamCode_, uid, json).subscribe();
-                                            });
-                                            break;
-                                    }
                                     break;
 
+
                             case "ingameSession" :
-
                                 log.info("start game");
-                                String teamCode = json.getString("teamCode");
-
-                                switch (event) {
-                                    case "startGame":
-                                        log.info("start game");
-                                        broadcasting.broadcasting(teamCode, json).subscribe();
-                                        break;
-
-                                    case "restartGame":
-                                        log.info("restart game");
-                                        String uid_ = json.getString("uid");
-                                        multicasting.multicasting(teamCode, uid_, json).subscribe();
-                                        break;
-                                }
+                                ingameSessionHandler.ingameHandler(json);
                                 break;
                         }
 
-                        return Mono.empty();
+                        return outbound.sendString(Mono.just("success"));   // echoing
 
                     } catch (JSONException e) {
                         log.error("ERROR : {}", e.getMessage());
