8dfb52e48ff7e7909d51eb9011e2d2eb9f9b9fbd
이주현 <ljhp1004@gmail.com>
2024-04-30 13:13:52 +0900

    [SERVER] feat: Add error handling for JSON parsing exception

--- server/chat/src/main/java/com/explorer/chat/servermanaging/RequestHandler.java
+++ server/chat/src/main/java/com/explorer/chat/servermanaging/RequestHandler.java
@@ -1,5 +1,7 @@
 package com.explorer.chat.servermanaging;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -20,7 +22,17 @@ public class RequestHandler {
                 .flatMap(msg -> {
 
                     log.info("Received message: {}", msg);
-                    return outbound.sendString(Mono.just(msg));
+
+                    try {
+
+                        return outbound.sendString(Mono.just(msg));
+
+                    } catch (JSONException e) {
+
+                        log.error("ERROR: {}", e.getMessage());
+                        return Mono.empty();
+
+                    }
 
                 })
                 .then();
