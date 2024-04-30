e612ea02bd29baf701f84ea7efe0e5be83cc8b2e
전재완 <wodhksdl7893@naver.com>
2024-04-30 10:08:42 +0900

    [SERVER] feat: redis 데이터 teamCode에서 channelId값으로 key값 변경 로직

--- server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/StartGame.java
+++ server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/StartGame.java
@@ -6,13 +6,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
-@Component
+@Service
 @RequiredArgsConstructor
 public class StartGame {
 
@@ -30,12 +30,21 @@ public class StartGame {
             memberIdsLong.add(Long.parseLong((String) memberId));
         }
         Channel channel = new Channel(channelId, channelName, memberIdsLong);
-        channelMongoRepository.save(channel);
+        Channel channel1 = channelMongoRepository.save(channel);
+        log.info("channel : {}", channel1.getId());
+        updateRedis(channelId, memberIds, teamCode);
     }
 
     private String createChannelId() {
         ObjectId newChannelId = new ObjectId();
         return newChannelId.toHexString();
+    }
+
+    private void updateRedis(String channelId, List<String> memberIds, String teamCode) {
+        String newKey = "channel:" + channelId;
+
+        redisListTemplate.opsForList().rightPushAll(newKey, memberIds.toArray(new String[0]));
+        redisListTemplate.delete("teamCode:" + teamCode);
     }
 
 }
