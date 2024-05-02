93cecacc9ca14ed3cfb4d7d3d9c3da4074bec353
전재완 <wodhksdl7893@naver.com>
2024-05-02 12:08:03 +0900

    [SERVER] feat: 게임 시작 클릭 시 redis에 저장된 connection, channel, user데이터 변경 로직

--- server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/StartGame.java
+++ server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/StartGame.java
@@ -5,12 +5,14 @@ import com.explorer.realtime.sessionhandling.ingame.repository.ChannelMongoRepos
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
+import org.springframework.data.redis.core.ReactiveHashOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -19,19 +21,21 @@ public class StartGame {
 
     private final ChannelMongoRepository channelMongoRepository;
 
-    private final RedisTemplate<String, String > redisListTemplate;
+    private final RedisTemplate<String, String > redisTemplate;
+
+    private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
 
 
     public void process(String teamCode, String channelName) {
         log.info("Processing game start for teamCode: {}", teamCode);
         String channelId = createChannelId();
-        Set<String> memberIds = redisListTemplate.opsForSet().members("channel:" + teamCode);
+        Set<String> userIds = redisTemplate.opsForSet().members("channel:" + teamCode);
 
-        log.info("Redis teamCode {}: {}", teamCode, memberIds);
+        log.info("Redis teamCode {}: {}", teamCode, userIds);
 
-        List<Long> memberIdsLong = new ArrayList<>();
+        Set<Long> memberIdsLong = new HashSet<>();
         try{
-            for (Object memberId : memberIds) {
+            for (Object memberId : userIds) {
                 memberIdsLong.add(Long.parseLong((String) memberId));
         }
         } catch (NumberFormatException e) {
@@ -41,7 +45,8 @@ public class StartGame {
         Channel channel = new Channel(channelId, channelName, memberIdsLong);
         Channel channel1 = channelMongoRepository.save(channel);
         log.info("channel : {}", channel1.getId());
-        updateRedis(channelId, new ArrayList<>(memberIds), teamCode);
+        updateConnection(teamCode, channelId).subscribe();
+        updateRedis(channelId, new HashSet<>(userIds), teamCode);
     }
 
     private String createChannelId() {
@@ -49,13 +54,27 @@ public class StartGame {
         return newChannelId.toHexString();
     }
 
-    private void updateRedis(String channelId, List<String> memberIds, String teamCode) {
+    private void updateRedis(String channelId, Set<String> memberIds, String teamCode) {
         String newKey = "channel:" + channelId;
 
-        redisListTemplate.opsForList().rightPushAll(newKey, memberIds.toArray(new String[0]));
-        redisListTemplate.delete("channel:" + teamCode);
+        redisTemplate.opsForSet().add(newKey, memberIds.toArray(new String[0]));
+        redisTemplate.delete("channel:" + teamCode);
     }
 
+    private Mono<Void> updateConnection(String teamCode, String channel) {
+        log.info("updateconnection", teamCode);
+        ReactiveHashOperations<String, String, String> hashOps = reactiveRedisTemplate.opsForHash();
+        return hashOps.entries(teamCode)
+                .collectList()
+                .flatMap(userId -> {
+                    if (userId.isEmpty()) {
+                        return Mono.error(new RuntimeException("Key not found or no entries in hash"));
+                    }
+                    return hashOps.putAll(channel, userId.stream().collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)))
+                            .then(Mono.defer(() -> reactiveRedisTemplate.delete(teamCode)))
+                                    .then();
+                });
+    }
 }
 
 
