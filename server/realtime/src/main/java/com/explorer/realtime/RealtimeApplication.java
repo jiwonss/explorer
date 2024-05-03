ea1ebea1f3666ae39a663b44908c5a4907821f94
서지원 <sjs3326@naver.com>
2024-05-03 11:18:45 +0900

    [SERVER] fix: config 관련 요류 해결 @ConfigurationPropertiesScan 추가

--- server/realtime/src/main/java/com/explorer/realtime/RealtimeApplication.java
+++ server/realtime/src/main/java/com/explorer/realtime/RealtimeApplication.java
@@ -2,10 +2,12 @@ package com.explorer.realtime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
 @EnableFeignClients(basePackages = "com.explorer.realtime")
+@ConfigurationPropertiesScan
+@SpringBootApplication
 public class RealtimeApplication {
 
     public static void main(String[] args) {
