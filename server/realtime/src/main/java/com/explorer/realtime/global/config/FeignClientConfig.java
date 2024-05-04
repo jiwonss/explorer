85c6f420abca548580fefd4838ac4a788427942d
서지원 <sjs3326@naver.com>
2024-05-04 18:19:12 +0900

    [SERVER] refactor: feign client config 이름 변경

--- server/realtime/src/main/java/com/explorer/realtime/global/config/FeignClientConfig.java
+++ server/realtime/src/main/java/com/explorer/realtime/global/config/FeignClientConfig.java
@@ -1,11 +1,16 @@
 package com.explorer.realtime.global.config;
 
+import feign.Feign;
 import feign.Logger;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
 
 @Configuration
-public class FeignConfig {
+public class FeignClientConfig {
 
     @Bean
     Logger.Level feignLoggerLevel() {
