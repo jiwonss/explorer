cdf47d988c5fbc0778030e6d1b14d1201f8ad20e
서지원 <sjs3326@naver.com>
2024-05-04 18:17:40 +0900

    [SERVER] refactor: UserClient에서 사용하는 API 변경

--- server/realtime/src/main/java/com/explorer/realtime/channeldatahandling/client/UserClient.java
+++ server/realtime/src/main/java/com/explorer/realtime/channeldatahandling/client/UserClient.java
@@ -1,7 +1,6 @@
 package com.explorer.realtime.channeldatahandling.client;
 
 import com.explorer.realtime.channeldatahandling.dto.Message;
-import com.explorer.realtime.channeldatahandling.dto.UserInfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -9,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 @FeignClient(value = "api-gateway", url = "${client.api-gateway.url}")
 public interface UserClient {
 
-    @GetMapping("/user/users/profile")
-    Message<UserInfo> getUserInfo(@RequestHeader("Authorization") String accessToken);
+    @GetMapping("/user/users/info")
+    Message<?> getUserInfo(@RequestHeader("Authorization") String accessToken);
 
 }
