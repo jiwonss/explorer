04f67ed99b93e2b7cc213531b27efe855ae87f64
이주현 <ljhp1004@gmail.com>
2024-05-07 05:52:19 +0900

    [SERVER] refactor: Update server initializer to handle requests using RequestHandler

--- server/logic/src/main/java/com/explorer/logic/servermanaging/ServerInitializer.java
+++ server/logic/src/main/java/com/explorer/logic/servermanaging/ServerInitializer.java
@@ -1,21 +1,26 @@
 package com.explorer.logic.servermanaging;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import reactor.netty.DisposableServer;
 import reactor.netty.http.server.HttpServer;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class ServerInitializer {
 
-    @Value("${server.port:1373}")
-    private static int PORT;
+    private final RequestHandler requestHandler;
+
+    private static final int PORT = 1379;
 
     public Mono<? extends DisposableServer> initializeServer() {
             return HttpServer
                     .create()
                     .port(PORT)
+                    .handle(requestHandler::handleRequest)
                     .bind();
     }
 }
