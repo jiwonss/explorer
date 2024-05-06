51d378fd255246b7c63709ba045a6c742989d9ad
서지원 <sjs3326@naver.com>
2024-05-06 15:32:35 +0900

    [SERVER] feat: 대기방 움직임 이벤트 핸들러 구현 테스트용

---
+++ server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/moving/event/Moving.java
@@ -0,0 +1,42 @@
+package com.explorer.realtime.gamedatahandling.moving.event;
+
+import com.explorer.realtime.global.common.dto.Message;
+import com.explorer.realtime.global.common.enums.CastingType;
+import com.explorer.realtime.global.component.broadcasting.Broadcasting;
+import com.explorer.realtime.global.util.MessageConverter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class Moving {
+
+    private final Broadcasting broadcasting;
+
+    public Mono<Void> process(JSONObject json) {
+        String channelId = json.getString("channelId");
+        int mapId = json.getInt("mapId");
+        Long userId = json.getLong("userId");
+        String position = json.getString("position");
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("mapId", mapId);
+        map.put("userId", userId);
+        map.put("position", position);
+
+        broadcasting.broadcasting(
+                channelId,
+                MessageConverter.convert(Message.success("moving", CastingType.BROADCASTING, map))
+        ).subscribe();
+
+        return Mono.empty();
+    }
+
+}
