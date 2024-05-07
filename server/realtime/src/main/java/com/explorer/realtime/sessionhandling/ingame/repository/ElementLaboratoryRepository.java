7e9bf4402687a192004a0a72e2d6b803a12c76fd
이주현 <ljhp1004@gmail.com>
2024-05-07 16:17:01 +0900

    [SERVER] feat: Add initialization of lists in `ElementLaboratoryRepository`

---
+++ server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/repository/ElementLaboratoryRepository.java
@@ -0,0 +1,37 @@
+package com.explorer.realtime.sessionhandling.ingame.repository;
+
+import org.springframework.data.redis.core.ReactiveListOperations;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+
+@Repository
+public class ElementLaboratoryRepository {
+
+    private final ReactiveListOperations<String, Object> listOperations;
+
+    private static final String KEY_PREFIX = "labData:";
+    private static final String ELEMENT_SUFFIX = ":0:element";
+    private static final String COMPOUND_SUFFIX = ":0:compound";
+
+    public ElementLaboratoryRepository(ReactiveRedisTemplate<String, Object> reactiveRedisTemplate) {
+        this.listOperations = reactiveRedisTemplate.opsForList();
+    }
+
+    public Mono<Void> initialize(String channelId) {
+        Object[] initialElements = new Object[10];
+        Arrays.fill(initialElements, 0);
+
+        Object[] initialCompounds = new Object[13];
+        Arrays.fill(initialCompounds, 0);
+
+        String elementKey = KEY_PREFIX+channelId+ELEMENT_SUFFIX;
+        String compoundKey = KEY_PREFIX + channelId + COMPOUND_SUFFIX;
+
+        return listOperations.rightPushAll(elementKey, Arrays.asList(initialElements))
+                .then(listOperations.rightPushAll(compoundKey, Arrays.asList(initialCompounds)))
+                .then();
+    }
+}
