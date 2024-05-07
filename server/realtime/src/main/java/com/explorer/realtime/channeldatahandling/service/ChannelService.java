51e691b7ffacadd2293fde4e9401b12a319e90bc
서지원 <sjs3326@naver.com>
2024-05-08 01:29:25 +0900

    [SERVER] feat: 채널 상세조회 구현

--- server/realtime/src/main/java/com/explorer/realtime/channeldatahandling/service/ChannelService.java
+++ server/realtime/src/main/java/com/explorer/realtime/channeldatahandling/service/ChannelService.java
@@ -1,12 +1,15 @@
 package com.explorer.realtime.channeldatahandling.service;
 
 import com.explorer.realtime.channeldatahandling.dto.ChannelInfo;
+import com.explorer.realtime.sessionhandling.ingame.document.Channel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -30,6 +33,12 @@ public class ChannelService {
 
         return reactiveMongoTemplate.aggregate(aggregation, "channels", ChannelInfo.class)
                 .collectList();
+    }
+
+    public Flux<Long> findPlayerListByChannelId(String channelId) {
+        Query query = new Query(Criteria.where("_id").is(channelId));
+        return reactiveMongoTemplate.findOne(query, Channel.class)
+                .flatMapMany(channel -> Flux.fromIterable(channel.getPlayerList()));
     }
 
 }
