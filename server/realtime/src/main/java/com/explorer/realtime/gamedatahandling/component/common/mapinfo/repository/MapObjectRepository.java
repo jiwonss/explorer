7c7a7108a4e8216922478bf4b5275399080dde52
전재완 <wodhksdl7893@naver.com>
2024-05-07 16:09:14 +0900

    [SERVER] feat: 맵 랜던 오브젝트 관련 데이터 hash 데이터 구조로 저장하는 repository

--- server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/component/common/mapinfo/repository/MapObjectRepository.java
+++ server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/component/common/mapinfo/repository/MapObjectRepository.java
@@ -1,25 +1,52 @@
 package com.explorer.realtime.gamedatahandling.component.common.mapinfo.repository;
 
+import org.json.JSONObject;
+import org.springframework.data.redis.core.ReactiveHashOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.ReactiveValueOperations;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
+import java.util.*;
+
 @Repository
 public class MapObjectRepository {
 
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
-    private final ReactiveValueOperations<String, String> reactiveValueOperations;
+    private final ReactiveHashOperations<String, String, String> hashOperations;
 
     private static final String KEY_PREFIX = "mapData";
 
     public MapObjectRepository(ReactiveRedisTemplate<String, String> reactiveRedisTemplate) {
         this.reactiveRedisTemplate = reactiveRedisTemplate;
-        this.reactiveValueOperations = reactiveRedisTemplate.opsForValue();
+        this.hashOperations = reactiveRedisTemplate.opsForHash();
     }
 
-    public Mono<Boolean> saveMapData(String channelId, Integer mapId, String data) {
+    public Mono<Boolean> saveMapData(String channelId, Integer mapId, List<String> positions, Integer category, Integer itemId) {
+        Map<String, String> hashData = dataToHash(positions, category, itemId);
         String key = KEY_PREFIX + ":" + channelId + ":" + mapId;
-        return reactiveValueOperations.set(key, data);
+        return hashOperations.putAll(key, hashData)
+                .map(result -> result == Boolean.TRUE);
+    }
+
+    private List<String> selectRandomEntries(List<String> positions, int count) {
+        List<String> selectedEntries = new ArrayList<>(positions);
+        Collections.shuffle(selectedEntries);  // 리스트를 무작위로 섞음
+        return selectedEntries.subList(0, Math.min(selectedEntries.size(), count));  // 랜덤하게 count개의 요소를 선택
+    }
+
+    private Map<String, String> dataToHash(List<String> positions, Integer category, Integer itemId) {
+        Map<String, String> hashData = new HashMap<>();
+        for (String position : positions) {
+            hashData.put(position, category + ":" + itemId);
+        }
+        return hashData;
+    }
+
+    public Mono<java.util.Map<String, String>> findMapData(String channelId, Integer mapId) {
+        String key = KEY_PREFIX + ":" + channelId + ":" + mapId;
+        return hashOperations.entries(key)
+                .collectMap(java.util.Map.Entry::getKey, java.util.Map.Entry::getValue);
     }
 }
+
