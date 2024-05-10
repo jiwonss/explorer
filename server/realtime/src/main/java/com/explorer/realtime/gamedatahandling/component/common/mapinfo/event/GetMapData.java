d8e6d0dba6d3a353660832cec3d41bd84c78ec8b
전재완 <wodhksdl7893@naver.com>
2024-05-10 17:28:37 +0900

    [SERVER] feat: 금성, 수성 맵 탐사 시작 시 redis에서 데이터 받아오는 로직

---
+++ server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/component/common/mapinfo/event/GetMapData.java
@@ -0,0 +1,25 @@
+package com.explorer.realtime.gamedatahandling.component.common.mapinfo.event;
+
+import com.explorer.realtime.gamedatahandling.component.common.mapinfo.repository.MapObjectRepository;
+import com.explorer.realtime.global.common.dto.Message;
+import com.explorer.realtime.global.common.enums.CastingType;
+import com.explorer.realtime.global.component.broadcasting.Broadcasting;
+import com.explorer.realtime.global.util.MessageConverter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class GetMapData {
+
+    private final MapObjectRepository mapObjectRepository;
+    private final Broadcasting broadcasting;
+
+    public Mono<Void> getMapData(String channelId, Integer mapId) {
+        return mapObjectRepository.findMapData(channelId, mapId)
+                .flatMap(mapData -> broadcasting.broadcasting(channelId, MessageConverter.convert(Message.success("getMapData", CastingType.BROADCASTING, mapData))));
+    }
+}
