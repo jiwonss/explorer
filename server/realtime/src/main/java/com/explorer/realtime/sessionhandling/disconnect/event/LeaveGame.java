58429feeb243d2053b22c21ef9f47efde216d524
전재완 <wodhksdl7893@naver.com>
2024-05-12 21:19:05 +0900

    [SERVER] feat: 게임 종료 시 유저 인벤토리, 연구소 데이터 mongoDB에 저장 및 redis 데이터 삭제 로직 구현

--- server/realtime/src/main/java/com/explorer/realtime/sessionhandling/disconnect/event/LeaveGame.java
+++ server/realtime/src/main/java/com/explorer/realtime/sessionhandling/disconnect/event/LeaveGame.java
@@ -1,13 +1,18 @@
 package com.explorer.realtime.sessionhandling.disconnect.event;
 
 import com.explorer.realtime.gamedatahandling.component.common.mapinfo.repository.MapObjectRepository;
+import com.explorer.realtime.gamedatahandling.component.personal.inventoryInfo.repository.InventoryInfoRepository;
 import com.explorer.realtime.gamedatahandling.component.personal.playerInfo.repository.PlayerInfoRepository;
+import com.explorer.realtime.gamedatahandling.laboratory.repository.ElementLaboratoryRepository;
 import com.explorer.realtime.global.common.dto.Message;
 import com.explorer.realtime.global.common.enums.CastingType;
 import com.explorer.realtime.global.component.broadcasting.Broadcasting;
 import com.explorer.realtime.global.component.session.SessionManager;
+import com.explorer.realtime.global.mongo.entity.Inventory;
+import com.explorer.realtime.global.mongo.entity.InventoryData;
 import com.explorer.realtime.global.mongo.entity.MapData;
 import com.explorer.realtime.global.mongo.entity.PositionData;
+import com.explorer.realtime.global.mongo.repository.InventoryDataMongoRepository;
 import com.explorer.realtime.global.mongo.repository.MapDataMongoRepository;
 import com.explorer.realtime.global.redis.ChannelRepository;
 import com.explorer.realtime.global.util.MessageConverter;
@@ -32,12 +37,18 @@ public class LeaveGame {
     private final Broadcasting broadcasting;
     private final SessionManager sessionManager;
     private final PlayerInfoRepository playerInfoRepository;
+    private final InventoryInfoRepository inventoryInfoRepository;
+    private final InventoryDataMongoRepository inventoryDataMongoRepository;
+    private final SaveLabData saveLabData;
+    private final ElementLaboratoryRepository elementLaboratoryRepository;
 
     public Mono<Void> process(String channelId, Long userId) {
         log.info("Leave game");
         List<Integer> mapIds = Arrays.asList(1, 2, 3);
         Map<String, String> map = new HashMap<>();
         map.put("userId", String.valueOf(userId));
+        saveInventoryData(channelId, userId).subscribe();
+        saveLabData.process(channelId).subscribe();
 
         return Flux.fromIterable(mapIds)
                 .flatMap(mapId -> mapObjectRepository.findMapData(channelId, mapId)
@@ -52,7 +63,7 @@ public class LeaveGame {
                             mapData.setChannelId(channelId);
                             mapData.setMapId(mapId);
                             List<PositionData> positions = new ArrayList<>();
-log.info("Leave game start {}", positions);
+                            log.info("Leave game start {}", positions);
                             data.forEach((position, value) -> {
                                 String[] parts = value.split(":");
                                 String itemCategory = parts[0];
@@ -88,6 +99,7 @@ log.info("Leave game start {}", positions);
     private Mono<Boolean> deleteData(String channelId, Long userId) {
         playerInfoRepository.deleteUserChannelInfo(channelId, userId).subscribe();
         userRepository.delete(userId).subscribe();
+        inventoryInfoRepository.deleteUserInventory(channelId, userId).subscribe();
         channelRepository.deleteByUserId(channelId, userId).subscribe();
         return Mono.empty();
     }
@@ -96,6 +108,37 @@ log.info("Leave game start {}", positions);
         channelRepository.deleteAll(channelId).subscribe();
         mapObjectRepository.deleteAllMap(channelId).subscribe();
         sessionManager.removeConnection(userId);
+        elementLaboratoryRepository.deleteAllData(channelId).subscribe();
         return Mono.empty();
+    }
+
+    private Mono<Boolean> saveInventoryData(String channelId, Long userId) {
+        return inventoryInfoRepository.findInventoryData(channelId, userId)
+                .switchIfEmpty(Mono.defer(() -> {
+                    log.warn("No data found");
+                    return Mono.empty();
+                }))
+                .doOnNext(data -> log.info("data found for mapId {}: {}", channelId, data))
+                .doOnError(error -> log.error("error {} : {}", channelId, error))
+                .flatMap(data -> {
+                    Inventory inventory = new Inventory();
+                    inventory.setChannelId(channelId);
+                    inventory.setUserId(userId);
+                    List<InventoryData> inventoryDataList = new ArrayList<>();
+                    log.info("Leave game start {}", inventoryDataList);
+                    data.forEach((inventoryIdx, value) -> {
+                        String[] parts = value.split(":");
+                        String itemCategory = parts[0];
+                        Integer itemId = Integer.parseInt(parts[1]);
+                        Integer itemCnt = Integer.parseInt(parts[2]);
+                        String isFull = parts[3];
+                        inventoryDataList.add(new InventoryData(Integer.parseInt(inventoryIdx), itemCategory, itemId, itemCnt, isFull));
+                    });
+
+                    inventory.setInventoryData(inventoryDataList);
+                    log.info("Leave game end {}", inventoryDataList);
+                    inventoryDataMongoRepository.save(inventory).subscribe();
+                    return Mono.empty();
+                });
     }
 }
