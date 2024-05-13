e41ab3eee14624dc0944540602733b92a1561306
이주현 <ljhp1004@gmail.com>
2024-05-13 17:51:11 +0900

    [SERVER] feat: 연구실 관련 이벤트 처리 핸들러에 연구소 업그레이드 로직 추가

--- server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/laboratory/LaboratoryHandler.java
+++ server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/laboratory/LaboratoryHandler.java
@@ -2,6 +2,7 @@ package com.explorer.realtime.gamedatahandling.laboratory;
 
 import com.explorer.realtime.gamedatahandling.laboratory.event.Extract;
 import com.explorer.realtime.gamedatahandling.laboratory.event.Synthesize;
+import com.explorer.realtime.gamedatahandling.laboratory.event.Upgrade;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
@@ -15,6 +16,7 @@ public class LaboratoryHandler {
 
     private final Extract extract;
     private final Synthesize synthesize;
+    private final Upgrade upgrade;
 
     public Mono<Void> laboratoryHandler(JSONObject json) {
         String eventName = json.getString("eventName");
@@ -27,6 +29,10 @@ public class LaboratoryHandler {
             case "synthesizing":
                 log.info("eventName : {}", eventName);
                 synthesize.process(json).subscribe();
+                break;
+            case "upgrade":
+                log.info("eventName : {}", eventName);
+                upgrade.process(json).subscribe();
                 break;
         }
         return Mono.empty();
