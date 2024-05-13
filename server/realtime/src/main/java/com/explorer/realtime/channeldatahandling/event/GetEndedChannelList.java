8b6afbe803340b96cdb06e7b14dc38c71857d98e
서지원 <sjs3326@naver.com>
2024-05-14 03:41:36 +0900

    [SERVER] feat: 종료된 행성 리스트 조회 구현

--- server/realtime/src/main/java/com/explorer/realtime/channeldatahandling/event/GetEndedChannelList.java
+++ server/realtime/src/main/java/com/explorer/realtime/channeldatahandling/event/GetEndedChannelList.java
@@ -1,10 +1,12 @@
 package com.explorer.realtime.channeldatahandling.event;
 
-import com.explorer.realtime.channeldatahandling.client.UserClient;
+import com.explorer.realtime.channeldatahandling.dto.ChannelDetailsInfo;
+import com.explorer.realtime.channeldatahandling.dto.PlayerInfo;
 import com.explorer.realtime.channeldatahandling.service.ChannelService;
 import com.explorer.realtime.global.common.dto.Message;
 import com.explorer.realtime.global.common.enums.CastingType;
 import com.explorer.realtime.global.component.broadcasting.Unicasting;
+import com.explorer.realtime.global.redis.ChannelRepository;
 import com.explorer.realtime.global.util.MessageConverter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,23 +15,23 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 import reactor.netty.Connection;
 
+import java.util.List;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class GetChannelList {
+public class GetEndedChannelList {
 
-    private final UserClient userClient;
     private final ChannelService channelService;
     private final Unicasting unicasting;
 
-    private static final String TOKEN_PREFIX = "Bearer ";
-    private static final String eventName = "getChannelList";
+    private static final String eventName = "getEndedChannelList";
 
     public Mono<Void> process(JSONObject json, Connection connection) {
         Long userId = json.getLong("userId");
         log.info("[process] userId : {}, connection : {}", userId, connection);
 
-        return channelService.findAllInProgressChannelInfoByUserId(userId)
+        return channelService.findAllEndedChannelInfoByUserId(userId)
                 .doOnNext(channels -> {
                     log.info("[process] channels : {}", channels);
                     unicasting.unicasting(
@@ -39,9 +41,6 @@ public class GetChannelList {
                                     Message.success(eventName, CastingType.UNICASTING, channels)
                             )
                     ).subscribe();
-                })
-                .onErrorResume(error -> {
-                    return Mono.empty();
                 })
                 .then();
     }
--- server/realtime/src/main/java/com/explorer/realtime/channeldatahandling/service/ChannelService.java
+++ server/realtime/src/main/java/com/explorer/realtime/channeldatahandling/service/ChannelService.java
@@ -2,12 +2,14 @@ package com.explorer.realtime.channeldatahandling.service;
 
 import com.explorer.realtime.channeldatahandling.dto.ChannelDetailsInfo;
 import com.explorer.realtime.channeldatahandling.dto.ChannelInfo;
+import com.explorer.realtime.channeldatahandling.dto.EndedChannelInfo;
 import com.explorer.realtime.sessionhandling.ingame.document.Channel;
 import com.explorer.realtime.sessionhandling.ingame.dto.UserInfo;
 import com.explorer.realtime.sessionhandling.ingame.enums.Status;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.DateOperators;
 import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -25,7 +27,7 @@ public class ChannelService {
 
     private final ReactiveMongoTemplate reactiveMongoTemplate;
 
-    public Mono<List<ChannelInfo>> findAllChannelInfoByUserId(Long userId) {
+    public Mono<List<ChannelInfo>> findAllInProgressChannelInfoByUserId(Long userId) {
         ProjectionOperation project = Aggregation.project()
                 .andExpression("_id").as("channelId")
                 .andExpression("name").as("channelName");
@@ -37,6 +39,25 @@ public class ChannelService {
         );
 
         return reactiveMongoTemplate.aggregate(aggregation, "channels", ChannelInfo.class)
+                .collectList();
+    }
+
+    public Mono<List<EndedChannelInfo>> findAllEndedChannelInfoByUserId(Long userId) {
+        ProjectionOperation project = Aggregation.project()
+                .andExpression("_id").as("channelId")
+                .andExpression("name").as("channelName")
+                .andExpression("image").as("image")
+                .and(DateOperators.DateToString.dateOf("createdAt").toString("%Y/%m/%d %H:%M")).as("createdAt")
+                .and(DateOperators.DateToString.dateOf("updatedAt").toString("%Y/%m/%d %H:%M")).as("endedAt")
+                .and("playerList.nickname").as("playerList");
+
+        Aggregation aggregation = Aggregation.newAggregation(
+                Aggregation.match(Criteria.where("playerList.userId").is(userId)),
+                Aggregation.match(Criteria.where("status").is(Status.ENDED)),
+                project
+        );
+
+        return reactiveMongoTemplate.aggregate(aggregation, "channels", EndedChannelInfo.class)
                 .collectList();
     }
 
