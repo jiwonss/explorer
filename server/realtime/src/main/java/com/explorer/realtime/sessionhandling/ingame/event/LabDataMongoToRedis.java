92580b226dac7057dc9f43d73bef2408ff4cd7bd
전재완 <wodhksdl7893@naver.com>
2024-05-13 13:13:56 +0900

    [SERVER] feat: 게임 재 시작 시 mongoDB에 저장된 연구소 데이터 redis로 저장을 위한 로직

---
+++ server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/event/LabDataMongoToRedis.java
@@ -0,0 +1,50 @@
+package com.explorer.realtime.sessionhandling.ingame.event;
+
+import com.explorer.realtime.gamedatahandling.laboratory.repository.ElementLaboratoryRepository;
+import com.explorer.realtime.global.mongo.repository.LaboratoryDataMongoRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class LabDataMongoToRedis {
+
+    private final LaboratoryDataMongoRepository laboratoryDataMongoRepository;
+    private final ElementLaboratoryRepository elementLaboratoryRepository;
+    private final MapDataMongoToRedis mapDataMongoToRedis;
+
+    public Mono<Void> process(String channelId, String itemCategory) {
+        log.info("init process");
+        return elementLaboratoryRepository.exist(channelId)
+                .flatMap(isExist -> {
+                    log.info("isExist {}", isExist);
+                    if (!isExist) {
+                        log.info("process start");
+                        mapDataMongoToRedis.process(channelId).subscribe();
+                        return findMongoData(channelId, itemCategory);
+                    }
+                    return Mono.empty();
+                });
+
+    }
+
+    private Mono<Void> saveToRedis(String key, List<Integer> itemCntList) {
+        return elementLaboratoryRepository.save(key, itemCntList);
+    }
+
+    private Mono<Void> findMongoData(String channelId, String itemCategory) {
+        log.info("mongo {}", laboratoryDataMongoRepository.findByChannelIdAndItemCategory(channelId, itemCategory).subscribe());
+        return laboratoryDataMongoRepository.findByChannelIdAndItemCategory(channelId, itemCategory)
+                .flatMap(laboratory -> {
+                    String key = "labData" + channelId + ":" + 0 + ":" + itemCategory;
+                    log.info("dataSave");
+                    return saveToRedis(key, laboratory.getItemCnt());
+//                    return Mono.empty();
+                });
+    }
+}
