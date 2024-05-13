f04fe96af0b7df64e721e02675e61327aabfb2e9
서지원 <sjs3326@naver.com>
2024-05-14 04:04:34 +0900

    [SERVER] feat: 엔딩 이벤트 헨들러 구현

--- server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/document/Channel.java
+++ server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/document/Channel.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 import java.util.Set;
 
 @Getter
+@Setter
 @Builder
 @ToString
 @NoArgsConstructor
--- server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/dto/UserInfo.java
+++ server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/dto/UserInfo.java
@@ -3,9 +3,11 @@ package com.explorer.realtime.sessionhandling.ingame.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @Builder
+@ToString
 @AllArgsConstructor
 public class UserInfo {
 
