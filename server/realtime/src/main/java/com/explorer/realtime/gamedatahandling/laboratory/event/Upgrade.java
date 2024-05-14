578e28fec61bbd95108a4ca023bfd6305a0d6dba
이주현 <ljhp1004@gmail.com>
2024-05-14 09:44:52 +0900

    [SERVER] feat: (Upgrade) 현재 연구소의 레벨을 조회한 후 로깅한다.

--- server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/laboratory/event/Upgrade.java
+++ server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/laboratory/event/Upgrade.java
@@ -1,5 +1,6 @@
 package com.explorer.realtime.gamedatahandling.laboratory.event;
 
+import com.explorer.realtime.gamedatahandling.laboratory.repository.InventoryLevelRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
@@ -15,13 +16,33 @@ public class Upgrade {
     @Value("${logic.laboratory.upgrade-url}")
     private String upgradeUrl;
 
+    private final InventoryLevelRepository inventoryLevelRepository;
+
     public Mono<Void> process(JSONObject json) {
 
-        return Mono.empty();
+        return checLabLevel(json)
+                .doOnNext(level -> log.info("Lab level: {}", level))
+                .then();
     }
 
-    private Mono<Void> checkLevel(JSONObject json) {
-        return Mono.empty();
+    /*
+     * [연구소의 레벨을 확인한다]
+     *
+     * 파라미터
+     * - 타입 : JSONObject
+     * - 값 : {..., "channelId":{channelId}, "userId":{userId}}
+     *
+     * 반환값
+     * - 타입 : Mono<Object>
+     * - 값 : {level}
+     *
+     * 연구소 레벨 데이터 (redis-game)
+     * key : labLevel:{channelId}:{labId}
+     * value: {level}
+     */
+    private Mono<Object> checLabLevel(JSONObject json) {
+        String channelId = json.getString("channelId");
+        return inventoryLevelRepository.findLabLevel(channelId, 0);
     }
 
 }
