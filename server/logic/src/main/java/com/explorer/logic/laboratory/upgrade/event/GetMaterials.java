71bd61d18756dfb33bd6f85a48c22a2340fdfd09
이주현 <ljhp1004@gmail.com>
2024-05-14 12:13:20 +0900

    [SERVER] feat: (Upgrade) 연구소 업그레이드에 필요한 재료를 조회한 후 Mono<String>으로 반환

---
+++ server/logic/src/main/java/com/explorer/logic/laboratory/upgrade/event/GetMaterials.java
@@ -0,0 +1,49 @@
+package com.explorer.logic.laboratory.upgrade.event;
+
+import com.explorer.logic.laboratory.upgrade.repository.UpgradeMaterialRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component("GetMaterialsForLabUpgrade")
+@RequiredArgsConstructor
+public class GetMaterials {
+
+    private final UpgradeMaterialRepository upgradeMaterialRepository;
+
+    /*
+     * [연구소 업그레이드를 위한 재료 반환]
+     * 파라미터 :
+     * - String body : {"labId":{labId}, "labLevel":{labLevel}}
+     */
+    public Mono<String> process(String body) {
+        log.info("Request body: {}", body);
+
+        try {
+            JSONObject json = new JSONObject(body);
+            JSONObject responseJson = new JSONObject();
+
+            int labId = json.getInt("labId");
+            int labLevel = json.getInt("labLevel");
+
+            return upgradeMaterialRepository.findAll(labId, labLevel+1)
+                    .flatMapMany(map -> Flux.fromIterable(map.entrySet())
+                            .doOnNext(entry -> {
+                                responseJson.put(entry.getKey().toString(), entry.getValue());
+                            })
+                    )
+                    .then(Mono.fromCallable(() -> {
+                        log.info("responseJson: {}", responseJson);
+                        return responseJson.toString();
+                    }));
+
+        } catch (Exception e) {
+            log.error("Error parsing JSON: {}", e.getMessage());
+            return Mono.just(new JSONObject().put("error", e.getMessage()).toString());
+        }
+    }
+}
