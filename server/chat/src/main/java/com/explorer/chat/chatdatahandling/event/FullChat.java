2bc5544df372ce4bffd5f91782b1ebfa1757b52f
문혜린 <9993hye@naver.com>
2024-05-14 10:20:38 +0900

    [SERVER] feat : 채팅 데이터 파싱 및 브로드캐스팅 구현

--- server/chat/src/main/java/com/explorer/chat/chatdatahandling/event/FullChat.java
+++ server/chat/src/main/java/com/explorer/chat/chatdatahandling/event/FullChat.java
@@ -1,6 +1,10 @@
 package com.explorer.chat.chatdatahandling.event;
 
+import com.explorer.chat.chatdatahandling.repository.UserRepository;
+import com.explorer.chat.global.common.dto.Message;
+import com.explorer.chat.global.common.enums.CastingType;
 import com.explorer.chat.global.component.broadcasting.Broadcasting;
+import com.explorer.chat.global.util.MessageConverter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
@@ -12,13 +16,30 @@ import reactor.netty.Connection;
 @Service
 @RequiredArgsConstructor
 public class FullChat {
+    private final UserRepository userRepository;
     private final Broadcasting broadcasting;
+
+    private static final String eventName = "chat";
 
     public Mono<Void> process(JSONObject json, Connection connection) {
         Long userId = json.getLong("userId");
+        String content = json.getString("content");
+        String teamCode = json.getString("teamCode");
         log.info("[process] userId : {}, connection : {}", userId, connection);
 
-        return Mono.empty();
+        return userRepository.findAll(userId)
+                .flatMap(map -> {
+                    String nickname = (String) map.get("nickname");
+                    JSONObject messageDataBody = new JSONObject();
+                    messageDataBody.put("nickname", nickname);
+                    messageDataBody.put("content", content);
+
+                    JSONObject jsonMessage = MessageConverter.convert(
+                            Message.success(eventName, CastingType.BROADCASTING, messageDataBody));
+
+                    return broadcasting.broadcasting(teamCode, jsonMessage);
+                })
+                .then();
     }
 
 }
