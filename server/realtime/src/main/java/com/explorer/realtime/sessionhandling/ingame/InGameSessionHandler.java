46460ffd143568c87e5268b990a073ccc2054617
전재완 <wodhksdl7893@naver.com>
2024-05-14 13:30:30 +0900

    [SERVER] rename: eventName 변경

--- server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/InGameSessionHandler.java
+++ server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/InGameSessionHandler.java
@@ -30,7 +30,7 @@ public class InGameSessionHandler {
         switch (eventName) {
             case "startGame" :
                 log.info("start game");
-                String teamCode = json.getString("channelId");
+                String teamCode = json.getString("teamCode");
                 String channelName = json.getString("channelName");
                 startGame.process(teamCode, channelName);
                 break;
