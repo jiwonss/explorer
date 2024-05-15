a901965a4b0869cc902180c43dbd58f797f02fe3
Joohyun <ljhp1004@gmail.com>
2024-05-16 03:05:51 +0900

    [SERVER] feat: (upgrade) 연구소 레벨업

--- server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/laboratory/repository/LaboratoryLevelRepository.java
+++ server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/laboratory/repository/LaboratoryLevelRepository.java
@@ -1,6 +1,7 @@
 package com.explorer.realtime.gamedatahandling.laboratory.repository;
 
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.ReactiveValueOperations;
@@ -30,18 +31,30 @@ public class LaboratoryLevelRepository {
      * value: {level}
      */
     public Mono<Object> findLabLevel(String channelId, int labId) {
-        log.info("find {} Lab LEVEL", labId);
         String redisKey = KEY_PREFIX + channelId + ":" + labId;
         return reactiveValueOperations.get(redisKey)
-                .doOnSuccess(value -> {
-                    if (value != null) {
-                        log.info("Found LabLevel for channel {} : {}", channelId, value);
-                    } else {
-                        log.warn("No LabLevel for channel {}", channelId);
-                    }
-                })
                 .doOnError(error -> log.error("Failed to find LabLevel for channel {}", channelId, error));
 
+    }
+
+    /*
+     * [연구소 레벨업]
+     * 연구소 레벨 데이터
+     * key : labLevel:{channelId}:{labId}
+     * value: {level}
+     */
+    public Mono<Void> incLabLevel(JSONObject json) {
+        String channelId = json.getString("channelId");
+        int labId = json.getInt("labId");
+        String redisKey = KEY_PREFIX + channelId + ":" + labId;
+
+        // Get the current level from Redis, increment it, and save it back
+        return reactiveValueOperations
+                .get(redisKey)
+                .defaultIfEmpty("0") // If there's no current level, start from "0"
+                .map(value -> Integer.parseInt(value.toString()))
+                .flatMap(level -> reactiveValueOperations.set(redisKey, level+1)) // Save the new level back to Redis
+                .then(); // Return an empty Mono<Void> to signal completion
     }
 
 }
