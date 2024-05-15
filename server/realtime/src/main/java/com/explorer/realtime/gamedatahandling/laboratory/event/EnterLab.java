c7249ce064f35cf9e9bcec19cf3e9d90e82e04cf
Joohyun <ljhp1004@gmail.com>
2024-05-15 16:16:57 +0900

    [SERVER] feat: (enterLab) redis-game에서 연구소 사용 중인 플레이어 정보 조회 후 로깅 (로깅 안됨)

--- server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/laboratory/event/EnterLab.java
+++ server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/laboratory/event/EnterLab.java
@@ -1,18 +1,42 @@
 package com.explorer.realtime.gamedatahandling.laboratory.event;
 
+import com.explorer.realtime.gamedatahandling.laboratory.repository.UseLaboratoryRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class EnterLab {
+
+    private final UseLaboratoryRepository useLaboratoryRepository;
 
     public Mono<Void> process(JSONObject json) {
 
         log.info("EnterLab process start...");
 
-        return Mono.empty();
+        /*
+         * 1) 해당 연구소를 사용하고 있는 사람이 있는지 확인
+         */
+        return findPlayersUsingLaboratory(json)
+                .flatMap(users -> {
+                    if(users.isEmpty()) {
+                        log.info("No one is currently using the laboratory");
+                    } else {
+                        log.warn("Laboratory is currently being used by: {}", users);
+                    }
+                    return Mono.empty();
+                })
+                .then();
+
+    }
+
+    private Mono<Map<Object, Object>> findPlayersUsingLaboratory(JSONObject json) {
+        return useLaboratoryRepository.findAll(json);
     }
 }
