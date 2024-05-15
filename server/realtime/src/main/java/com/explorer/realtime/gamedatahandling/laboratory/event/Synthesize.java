1d79b99dd458de7d9794004944b2b77f8844a432
Joohyun <ljhp1004@gmail.com>
2024-05-16 00:23:05 +0900

    [SERVER] refactor: 변수명 변경 findElement -> findMaterial

--- server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/laboratory/event/Synthesize.java
+++ server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/laboratory/event/Synthesize.java
@@ -88,7 +88,7 @@ public class Synthesize {
         // 합성에 필요한 재료 하나씩 원소 연구소에 있는지 확인
         return Flux.fromIterable(responseJson.keySet())
                 .flatMap(key ->
-                        elementLaboratoryRepository.findElement(userInfo.getChannelId(), key, responseJson.optInt(key, 0))
+                        elementLaboratoryRepository.findMaterial(userInfo.getChannelId(), key, responseJson.optInt(key, 0))
                                 .flatMap(found -> {
                                     log.info("Key: {}, Required Count: {}, Found: {}", key, responseJson.optInt(key, 0), found);
 
