c19a568ca58ab89e42aed61ccf31d9eaf9eecf31
서지원 <sjs3326@naver.com>
2024-05-16 11:38:36 +0900

    [SERVER] refactor: 인벤토리에 들어올 수 없는 아이템 exception 추가

--- server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/farming/exception/FarmingErrorCode.java
+++ server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/farming/exception/FarmingErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum FarmingErrorCode {
 
+    INVALID_ITEM_CATEGORY_IN_INVENTORY("인벤토리에 들어갈 수 없는 아이템입니다.", HttpStatus.BAD_REQUEST),
     EXCEEDING_CAPACITY("인벤토리가 가득 차서 아이템을 추가할 수 없습니다.", HttpStatus.CONFLICT);
 
     private final String message;
