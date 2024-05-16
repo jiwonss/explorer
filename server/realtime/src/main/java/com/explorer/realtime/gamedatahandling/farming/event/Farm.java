5921a2b9abfa74b254e5f71d7e5a9b861fb612a2
이주현 <ljhp1004@gmail.com>
2024-05-16 17:44:10 +0900

    [SERVER] refactor: (Farming) 파밍 성공 로직::redis-game map 데이터에서 파밍 오브젝트 삭제 및 브로드캐스팅

--- server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/farming/event/Farm.java
+++ server/realtime/src/main/java/com/explorer/realtime/gamedatahandling/farming/event/Farm.java
@@ -3,6 +3,7 @@ package com.explorer.realtime.gamedatahandling.farming.event;
 import com.explorer.realtime.gamedatahandling.farming.repository.MapInfoRepository;
 import com.explorer.realtime.global.common.dto.Message;
 import com.explorer.realtime.global.common.enums.CastingType;
+import com.explorer.realtime.global.component.broadcasting.Broadcasting;
 import com.explorer.realtime.global.component.broadcasting.Unicasting;
 import com.explorer.realtime.global.util.MessageConverter;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +22,11 @@ public class Farm {
 
     private final MapInfoRepository mapInfoRepository;
     private final Unicasting unicasting;
+    private final Broadcasting broadcasting;
 
+    /*
+     * 파라미터 JSONObject json: ... channelId, userId, mapId, position
+     */
     public Mono<Void> process(JSONObject json) {
 
         log.info("FARM process start...");
@@ -29,10 +34,12 @@ public class Farm {
         return checkItemInMap(json)
                 .flatMap(result -> {
                     if (result) {
-                        log.info("success");
-                        return Mono.empty();
+                        // 파밍 성공
+                        return farmItem(json)                           // redis-game map 상태 정보에서 파밍 오브젝트 삭제
+                                .then(broadcastingSuccessData(json));   // BROADCASTING: 파밍 성공
                     } else {
-                        return unicastingFailData(json, "noItem");
+                        // 파밍 실패
+                        return unicastingFailData(json, "noItem");  // UNICASTING: 파밍 실패
                     }
                 })
                 .doOnError(error -> log.error("ERROR farm process: {}", error.getMessage()))
@@ -61,6 +68,46 @@ public class Farm {
                 })
                 .defaultIfEmpty(false)
                 .doOnError(error -> log.error("ERROR finding item in map: {}", error.getMessage()));
+    }
+
+    /*
+     * [파밍 오브젝트 데이터 삭제 (redis-game)]
+     *
+     * redis-game의 map 상태 데이터 형식
+     * - key:  mapData:{channelId}:{mapId}
+     * - value (hash)
+     *   - field: {position}
+     *   - value: {itemCategory}:{isFarmable}:{itemId}:{itemCnt}
+     */
+    private Mono<Void> farmItem(JSONObject json) {
+        log.info("farmItem");
+        String channelId = json.getString("channelId");
+        int mapId = json.getInt("mapId");
+        String position = json.getString("position");
+
+        return mapInfoRepository.deleteByPosition(channelId, mapId, position)
+                .doOnSuccess(success -> log.info("SUCCESS delete farming object: {}", success))
+                .doOnError(error -> log.error("ERROR delete farming object: {}", error.getMessage()));
+    }
+
+    /*
+     * [파밍 성공 시 BROADCASTING]
+     * 파라미터 JSONObject json: ... channelId, userId, mapId, position
+     */
+    private Mono<Void> broadcastingSuccessData(JSONObject json) {
+
+        String channelId = json.getString("channelId");
+        int mapId = json.getInt("mapId");
+        String position = json.getString("position");
+
+        Map<Object, Object> dataBody = new HashMap<>();
+        dataBody.put("mapId", mapId);
+        dataBody.put("position", position);
+
+        return broadcasting.broadcasting(
+                channelId,
+                MessageConverter.convert(Message.success("farm", CastingType.BROADCASTING, dataBody))
+                ).then();
     }
 
     /*
