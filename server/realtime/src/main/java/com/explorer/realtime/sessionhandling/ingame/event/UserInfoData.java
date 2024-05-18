61415e5aed562d959bbace91c9f9ac730e9990f1
서지원 <sjs3326@naver.com>
2024-05-19 00:29:52 +0900

    [SERVER] fix: 게입 시작 브로드캐스팅 좌표 수정

--- server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/event/UserInfoData.java
+++ server/realtime/src/main/java/com/explorer/realtime/sessionhandling/ingame/event/UserInfoData.java
@@ -90,7 +90,9 @@ public class UserInfoData {
     }
 
     private String getNewPosition(Long userId) {
-        String[] positions = {"1:0:1", "2:0:2", "3:0:3", "1:0:2", "2:0:3", "1:0:3"};
+        String[] positions = {
+                "1:0:1:0:0:0", "2:0:2:0:0:0", "3:0:3:0:0:0",
+                "1:0:2:0:0:0", "2:0:3:0:0:0", "1:0:3:0:0:0"};
         int index = Math.abs(userId.hashCode()) % positions.length;
         return positions[index];
     }
