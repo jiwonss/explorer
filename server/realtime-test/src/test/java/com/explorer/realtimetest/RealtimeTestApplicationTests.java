6eb08de89ce7c0fe3a521084f0a257a359687d64
이주현 <ljhp1004@gmail.com>
2024-05-22 15:29:02 +0900

    [SERVER] feat: create realtime-test server for performance test

---
+++ server/realtime-test/src/test/java/com/explorer/realtimetest/RealtimeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.explorer.realtimetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
